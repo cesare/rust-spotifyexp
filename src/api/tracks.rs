f716473b053dbf9cbabd71518c5d54e6b5b1da9c
SAWADA Tadashi <cesare@mayverse.jp>
2021-06-25 22:12:24 +0900

    Separate tracks API into the module

--- src/api/tracks.rs
+++ src/api/tracks.rs
@@ -6,35 +6,34 @@ use reqwest::Client;
 use crate::config::SpotifyConfig;
 use crate::objects::*;
 
-mod player;
-pub use self::player::{enqueue_tracks, is_playing, skip_to_next, start_playing};
-
-mod search;
-pub use self::search::{SearchAlbums, SearchArtists};
-
-mod tracks;
-pub use self::tracks::{ListTracks};
-
-pub struct ListDevices {
+pub struct ListTracks {
     config: Rc<SpotifyConfig>,
+    album_id: String,
 }
 
-impl ListDevices {
-    pub fn new(config: &Rc<SpotifyConfig>) -> Self {
+impl ListTracks {
+    pub fn new(config: &Rc<SpotifyConfig>, album_id: &str) -> Self {
         Self {
             config: config.clone(),
+            album_id: album_id.to_owned(),
         }
     }
 
-    pub async fn execute(&self) -> Result<ListDevicesResponse> {
+    pub async fn execute(&self) -> Result<ListTracksResponse> {
         let client = Client::new();
-        let response = client.get("https://api.spotify.com/v1/me/player/devices")
+        let request_uri = format!("https://api.spotify.com/v1/albums/{}/tracks", self.album_id);
+        let parameters = [
+            ("market", "from_token"),
+            ("limit", "50"),
+        ];
+        let response = client.get(request_uri)
             .bearer_auth(&self.config.access_token)
+            .query(&parameters)
             .send()
             .await?;
 
         if response.status().is_success() {
-            response.json::<ListDevicesResponse>().await
+            response.json::<ListTracksResponse>().await
                 .with_context(|| "Failed to parse response")
         } else {
             let e = response.json::<ErrorResponse>().await?;
